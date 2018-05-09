270e18e62fece3d5cdb6fb7bcbd0b1119cc3e93b
Sebastian Lin <bctnry@outlook.com>
2018-05-10 00:11:49 +0800

    line_bresenham

--- Shape/line_bresenham_20180510.cpp
+++ Shape/line_bresenham_20180510.cpp
@@ -17,6 +17,8 @@
 static const int WINDOW_WIDTH = 640;
 static const int WINDOW_HEIGHT = 480;
 
+int abs(int a) { return a >= 0? a : -a; }
+
 void drawline(int, int, int, int);
 
 void init() {
@@ -83,6 +85,11 @@ void onMouse(int button, int state, int x, int y) {
     if((!right)&&(!mid)&&(!release)) { // left click
         lastX = x, lastY = y;
     }
+    if(right&&holddown) {
+        clear();
+        drawline(lastX, lastY, x, y);
+        glFlush();
+    }
 }
 
 
@@ -92,14 +99,75 @@ void onKey(unsigned char key, int x, int y) {
     // check using GLUT_ACTIVE_SHIFT/CTRL/ALT.
 }
 
+void drawpoint(int x, int y) {
+    glBegin(GL_POINTS); {
+        glVertex2f(x, y);
+    } glEnd();
+}
 
 // drawline func.
 void drawline(int x1, int y1, int x2, int y2) {
-    // TODO: change this to bresenham.
-    glBegin(GL_LINES); {
-        glVertex2f(x1, y1);
-        glVertex2f(x2, y2);
-    } glEnd();
+    int x, y, dx, dy, e;
+    dx = x2 - x1, dy = y2 - y1;
+
+    if(abs(dy) < abs(dx)) {
+        // if x1 is at the right of x2
+        // then swap points.
+        if(x1 > x2) {
+            int temp = x2; x2 = x1, x1 = temp;
+            temp = y2; y2 = y1, y1 = temp;
+            // and then modify dx & dy.
+            dx = -dx, dy = -dy;
+        }
+        /* |k| <= 1:
+           downwards: k < 0, e = 0.5, e += k,
+           if e <= 0 then y--; e += 1 (reset to 0.5);
+           else y same;
+           upwards: k > 0, e = -0.5, e += k,
+           if e >= 0 then y++; e -= 1 (reset to -0.5);
+           else y same;
+           here, e is actually 2edx --> dx,
+           and 1 is actually 2dx.
+        */
+        char updown = dy > 0? 1 : 0;
+        e = updown? -dx : dx;
+        x = x1, y = y1;
+        for(int i = 0; i <= dx; i++) {
+            drawpoint(x, y);
+            x++, e += 2 * dy;
+            if(updown? e >= 0 : e <= 0) {
+                y += updown? 1 : -1;
+                e += (updown? -1 : 1) * 2 * dx;
+            }
+        }
+    } else {
+        // now, |k| > 1, the role of y and x has changed.
+        // if y1 is at the top of y2
+        // then swap points.
+        if(y1 > y2) {
+            int temp = y2; y2 = y1, y1 = temp;
+            temp = x2; x2 = x1, x1 = temp;
+            dx = -dx, dy = -dy;
+        }
+        char updown = dx > 0? 1 : 0;
+        /*
+          upwards: k > 0, e = -0.5, e += 1/k = dx/dy,
+                   if e >= 0 then x++, e -= 1 (reset to -0.5)
+          downwards: k < 0, e = 0.5, e += 1/k = dx/dy,
+                     if e <= 0 then x--, e += 1 (reset to 0.5)
+          
+         */
+        e = updown? -dy : dy;
+        x = x1, y = y1;
+        for(int i = 0; i <= dy; i++) {
+            drawpoint(x, y);
+            y++, e += 2 * dx;
+            if(updown? e >= 0 : e <= 0) {
+                x += (updown? 1 : -1);
+                e += (updown? -1 : 1) * 2 * dy;
+            }
+        }
+    }
 }
 
 
